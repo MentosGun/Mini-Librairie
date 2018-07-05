b0f89164d8c3a98251d50ec1375623c09edbac73
MentosGun <mentosgun185@gmail.com>
2018-07-05 10:25:14 +0200

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import {User} from './entity/User';
+import {Room} from './entity/Room';
+import {Message} from './entity/Message';
+
+import{UserRepositoy} from './repository/UserRepositoy';
+
+export {
+  User,
+  Message,
+  Room,
+
+  UserRepositoy,
+}
